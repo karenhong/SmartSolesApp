37f8363eed0e9554081b19fbaf00248e5b4559a1
Karen Hong <karenhong4@gmail.com>
2020-01-25 23:47:29 -0800

    reorganize folders

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import Home from '../screens/home';
+import Home from '../src/screens/home';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
