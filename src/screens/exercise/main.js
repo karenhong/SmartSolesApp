4d30bacd540e92aca5b714003fca78efb11b6823
Karen Hong <karenhong4@gmail.com>
2020-03-15 11:57:28 -0700

    horizontal exercise scrolling

--- src/screens/exercise/main.js
+++ src/screens/exercise/main.js
@@ -3,14 +3,46 @@ import React from 'react';
 import ExerciseHeader from './header';
 import ExerciseCard from './exercise-card';
 
+import {FlatList, Text} from 'react-native';
 import {Container} from 'native-base';
 
+const exercises = [
+  {
+    text: 'Tippy Toes',
+    name: 'One',
+  },
+  {
+    text: 'Card One',
+    name: 'One',
+  },
+];
+
 class ExercisePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: exercises,
+    };
+  }
+
   render() {
     return (
       <Container>
         <ExerciseHeader />
-        <ExerciseCard />
+        <FlatList
+          horizontal
+          data={this.state.data}
+          renderItem={({item: rowData}) => {
+            return (
+              <ExerciseCard
+                title={null}
+                image={{uri: rowData.imageUrl}}>
+                <Text style={{marginBottom: 10}}>{rowData.title}</Text>
+              </ExerciseCard>
+            );
+          }}
+          keyExtractor={(item, index) => index}
+        />
       </Container>
     );
   }
